850a43e75bdaf18ef5e52777bbd9a91a9de83a8d
taehuiii <160212663+taehuiii@users.noreply.github.com>
2024-05-11 09:41:23 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -3,8 +3,6 @@
 SELECT A.NAME, A.DATETIME
 from ANIMAL_INS A Left join ANIMAL_OUTS B
 on A.ANIMAL_ID = B.ANIMAL_ID
-where B.DATETIME is null
+where b.ANIMAL_ID is null
 order by A.DATETIME asc
 Limit 3
-
-

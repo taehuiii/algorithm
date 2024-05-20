876479ad2cea112329749801e34a3f907386fc39
taehuiii <160212663+taehuiii@users.noreply.github.com>
2024-05-20 20:02:10 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,12 +1,9 @@
 class Solution {
     fun solution(s: String): Boolean {
-        var answer1 = false
-        answer1 = s.chars().allMatch { Character.isDigit(it) }
-        
-        var answer2 = false
-        if( s.length ==4 || s.length ==6){ answer2 = true}
-        
-       var answer :Boolean= answer1 && answer2
-        return answer
+      
+        if( s.all{it.isDigit()} && (s.length==4 || s.length==6)){
+            return true
+        }
+        return false
     }
 }

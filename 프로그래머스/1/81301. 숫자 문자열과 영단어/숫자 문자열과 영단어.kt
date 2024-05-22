4380b0bb28e9fd3b0b85df14e02dffa4f3d60180
taehuiii <160212663+taehuiii@users.noreply.github.com>
2024-05-22 15:22:25 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 7.60 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(s: String): Int {
+      return s.replace("zero", "0")
+            .replace("one","1")
+            .replace("two","2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five","5")
+            .replace("six", "6")
+            .replace("seven","7")
+            .replace("eight","8")
+            .replace("nine", "9")
+            .toInt()
+    }
+}
